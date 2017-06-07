c3b0df78d30bbec8feb07a435a85a9278cd33b21
gpdoud <gpdoud@gmail.com>
2017-06-07 16:29:45 -0400

    initial commit

---
+++ BreakTimerProject/Form1.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace BreakTimerProject {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.Timer = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Timer
+            // 
+            this.Timer.AutoSize = true;
+            this.Timer.Font = new System.Drawing.Font("Arial", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Timer.ImageAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.Timer.Location = new System.Drawing.Point(212, 122);
+            this.Timer.Name = "Timer";
+            this.Timer.Size = new System.Drawing.Size(503, 178);
+            this.Timer.TabIndex = 0;
+            this.Timer.Text = "label1";
+            this.Timer.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(916, 458);
+            this.Controls.Add(this.Timer);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Timer;
+    }
+}
+
---
+++ BreakTimerProject/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BreakTimerProject {
+
+    public partial class Form1 : Form {
+
+        Timer timer = new Timer();
+
+        public Form1() {
+            InitializeComponent();
+            timer.Tick += TimerTick;
+            timer.Start();
+        }
+
+        public void TimerTick(object sender, EventArgs args) {
+            var now = DateTime.Now.Ticks;
+            var then = new DateTime(2017, 06, 07, 15, 15, 00).Ticks;
+
+            this.Timer.Text = (new DateTime(then - now)).ToString("mm:ss");
+            var left = (this.Size.Width - this.Timer.Width) / 2;
+            var top = (this.Size.Height - this.Timer.Height) / 2 - (this.Timer.Height / 2);
+            this.Timer.Left = left;
+            this.Timer.Top = top;
+        }
+    }
+}
